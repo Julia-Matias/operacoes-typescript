80f732856d0e6008d93c9d0cb7556418690c500d
Julia Matias Reis <jmatiare@emeal.nttdata.com>
2023-07-31 16:37:17 -0300

    construcao controllers, enums, models e views

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,40 +1,55 @@
 import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from "../models/negociacoes.js";
+import { NegociacoesView } from "../views/negociacoes-view.js";
+import { MensagemView } from "../views/mensagem-view.js";
+import { DiasDaSemana } from "../enums/dias-da-semana.js";
 
 export class NegociacaoController {
   private inputData: HTMLInputElement;
   private inputQuantidade: HTMLInputElement;
   private inputValor: HTMLInputElement;
+  private negociacoes = new Negociacoes();
+  private negociacoesView = new NegociacoesView("#negociacoesView");
+  private mensagemView = new MensagemView("#mensagemView");
 
   constructor() {
-    this.inputData = document.querySelector("#data");
-    this.inputQuantidade = document.querySelector("#quantidade");
-    this.inputValor = document.querySelector("#valor");
+    this.inputData = <HTMLInputElement>document.querySelector("#data");
+    this.inputQuantidade = document.querySelector("#quantidade") as HTMLInputElement;
+    this.inputValor = document.querySelector("#valor") as HTMLInputElement;
+    this.negociacoesView.update(this.negociacoes);
   }
 
-  criaNegociacao(): Negociacao {
-    //OUTRA FORMA DE CONVERTER OS VALORES CONFORME A TIPAGEM DO CONSTRUTOR
-    // const regExp = /-/g;
-    // const date = new Date(this.inputData.value.replace(regExp, ","));
-    // const quantidade = parseInt(this.inputQuantidade.value);
-    // const valor = parseFloat(this.inputValor.value);
-    // return new Negociacao(date, quantidade, valor);
-    return new Negociacao(
-      this.inputData.valueAsDate,
-      this.inputQuantidade.valueAsNumber,
-      this.inputValor.valueAsNumber
+  public adiciona(): void {
+    const negociacao = Negociacao.criaDe(
+      this.inputData.value,
+      this.inputQuantidade.value,
+      this.inputValor.value
+    );
+    if (!this.diaUtil(negociacao.data)) {
+      this.mensagemView.update("Apenas negociações em dias úteis são aceitas!");
+      return;
+    }
+    this.negociacoes.adiciona(negociacao);
+    this.limparFormulario();
+    this.atualizaView();
+  }
+
+  private diaUtil(data: Date) {
+    return (
+      data.getDay() > DiasDaSemana.DOMINGO &&
+      data.getDay() < DiasDaSemana.SABADO
     );
   }
 
-  adiciona(): void {
-    const negociacao = this.criaNegociacao();
-    console.log(negociacao);
-    this.limparFormulario();
+  private limparFormulario(): void {
+    (this.inputData.value = ""),
+      (this.inputQuantidade.value = ""),
+      (this.inputValor.value = ""),
+      this.inputData.focus();
   }
 
-  limparFormulario(): void {
-    this.inputData.value = "",
-    this.inputQuantidade.value = "",
-    this.inputValor.value = "",
-    this.inputData.focus()
+  private atualizaView(): void {
+    this.negociacoesView.update(this.negociacoes);
+    this.mensagemView.update("Negociação adicionada com Sucesso!");
   }
 }
--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,27 +1,28 @@
 export class Negociacao {
-  private _data: Date;
-  private _quantidade: number;
-  private _valor: number;
+  constructor(
+    private _data: Date,
+    public readonly quantidade: number,
+    public readonly valor: number
+  ) {}
 
-  constructor(data: Date, quantidade: number, valor: number) {
-    this._data = data;
-    this._quantidade = quantidade;
-    this._valor = valor;
+  get volume(): number {
+    return this.quantidade * this.valor;
   }
 
   get data(): Date {
-    return this._data;
+    const data = new Date(this._data.getTime());
+    return data;
   }
 
-  get quantidade(): number {
-    return this._quantidade;
-  }
-
-  get valor(): number {
-    return this._valor;
-  }
-
-  get volume(): number {
-    return this._quantidade * this._valor;
+  public static criaDe(
+    dataString: string,
+    quantidadeString: string,
+    valorString: string
+  ): Negociacao {
+    const regExp = /-/g;
+    const date = new Date(dataString.replace(regExp, ","));
+    const quantidade = parseInt(quantidadeString);
+    const valor = parseFloat(valorString);
+    return new Negociacao(date, quantidade, valor);
   }
 }
